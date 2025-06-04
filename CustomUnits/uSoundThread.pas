12cad59f72b8b632464a4c67d4af217342b478e0
Adrian <66483092+serbinskis@users.noreply.github.com>
2025-06-04 19:00:10 +0300

    Update uSoundThread.pas

--- CustomUnits/uSoundThread.pas
+++ CustomUnits/uSoundThread.pas
@@ -3,41 +3,74 @@ unit uSoundThread;
 interface
 
 uses
-  Classes, MMSystem, Windows;
+  Classes, Windows, SysUtils, WavePlayers, TNTClasses;
 
 type
   TSoundThread = class(TThread)
   private
-    FFileName: WideString;
-    FFlags: DWORD;
+    FStream: TStream;
+    FAmount: Integer;
+    FAudioPlayer: TStockAudioPlayer;
   protected
     procedure Execute; override;
+    procedure StockAudioPlayerDeactivate(Sender: TObject);
+    constructor Create(Stream: TStream; Amount: Integer);
   public
-    constructor Create(const FileName: WideString; Flags: DWORD);
-    class procedure Play(const FileName: WideString; Flags: DWORD);
+    class function PlayStream(Stream: TStream; Amount: Integer): TSoundThread;
+    class function PlayFile(FileName: WideString; Amount: Integer): TSoundThread;
+    class function PlayResource(FileName: WideString; ResourceType: WideString; Amount: Integer): TSoundThread;
   end;
 
 implementation
 
-constructor TSoundThread.Create(const FileName: WideString; Flags: DWORD);
+constructor TSoundThread.Create(Stream: TStream; Amount: Integer);
 begin
   inherited Create(True);
   FreeOnTerminate := True;
-  FFileName := FileName;
-  FFlags := Flags;
+  FStream := Stream;
+  if (Amount > 0) then FAmount := Amount else FAmount := MaxInt;
+  FAudioPlayer := TStockAudioPlayer.Create(nil);
+  FAudioPlayer.OnDeactivate := self.StockAudioPlayerDeactivate;
+  self.Resume;
 end;
 
-class procedure TSoundThread.Play(const FileName: WideString; Flags: DWORD);
-var
-  Thread: TSoundThread;
+class function TSoundThread.PlayStream(Stream: TStream; Amount: Integer): TSoundThread;
 begin
-  Thread := TSoundThread.Create(FileName, Flags);
-  Thread.Resume;
+  Result := TSoundThread.Create(Stream, Amount);
+end;
+
+class function TSoundThread.PlayFile(FileName: WideString; Amount: Integer): TSoundThread;
+begin
+  Result := TSoundThread.Create(TTntFileStream.Create(FileName, fmOpenRead or fmShareDenyNone), Amount);
+end;
+
+class function TSoundThread.PlayResource(FileName: WideString; ResourceType: WideString; Amount: Integer): TSoundThread;
+begin
+  if (ResourceType = '') then ResourceType := 'WAVE';
+  Result := TSoundThread.Create(TTntResourceStream.Create(HInstance, FileName, PWideChar(ResourceType)), Amount);
+end;
+
+procedure TSoundThread.StockAudioPlayerDeactivate(Sender: TObject);
+begin
+  if (FAmount > 0) then begin
+    Dec(FAmount);
+    FAudioPlayer.PlayStream(FStream);
+  end else begin
+    FStream.Free;
+    self.Free;
+  end;
 end;
 
 procedure TSoundThread.Execute;
+var
+  Msg: TMsg;
 begin
-  PlaySoundW(PWideChar(FFileName), 0, SND_SYNC or FFlags);
+  StockAudioPlayerDeactivate(nil);
+
+  while GetMessage(Msg, 0,0,0) do begin
+    TranslateMessage(Msg);
+    DispatchMessage(Msg);
+  end;
 end;
 
 end.
