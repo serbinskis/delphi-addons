274b7bcf11c0b28707ef52077cb39ef0b24ca997
Adrian <66483092+serbinskis@users.noreply.github.com>
2025-06-04 16:16:33 +0300

    Update MSIController.pas

--- EmbeddedController/MSIController.pas
+++ EmbeddedController/MSIController.pas
@@ -54,7 +54,7 @@ const
 
 type
   TModeType = (modeAuto, modeBasic, modeAdvanced);
-  TScenarioType = (scenarioUnknow, scenarioSilent, scenarioBalanced, scenarioAuto, scenarioCoolerBoost, scenarioAdvanced);
+  TScenarioType = (scenarioUnknown, scenarioSilent, scenarioBalanced, scenarioAuto, scenarioCoolerBoost, scenarioAdvanced);
   TFanSpeedArray = array[0..5] of Integer;
   PFanSpeedArray = ^TFanSpeedArray;
 
