4911734e32a593604847457ac2cd649990816f11
Adrian <66483092+serbinskis@users.noreply.github.com>
2025-06-09 18:54:33 +0300

    Update MSIController.pas

--- EmbeddedController/MSIController.pas
+++ EmbeddedController/MSIController.pas
@@ -3,7 +3,7 @@ unit MSIController;
 interface
 
 uses
-  Windows, EmbeddedController;
+  Windows, StrUtils, SysUtils, ActiveX, ComObj, Variants, EmbeddedController;
 
 const
   EC_LOADED_RETRY = 20;
@@ -49,6 +49,7 @@ const //Vector 16 HX A14VHG
   EC_FAN_MODE_ADVANCED = $8D;
 
 const
+  MSI_BIOS_SERIAL_NUMBER_PREFIX = '9S7';
   EC_DEFAULT_CPU_FAN_SPEED: array[0..5] of Byte = (0, 40, 48, 60, 75, 89);
   EC_DEFAULT_GPU_FAN_SPEED: array[0..5] of Byte = (0, 48, 60, 70, 82, 93);
 
@@ -64,9 +65,10 @@ type
     protected
        EC: TEmbeddedController;
        hasEC: Boolean;
-       function readByte(bRegister: Byte): Byte;
-       procedure writeByte(bRegister, value: Byte); overload;
-       procedure writeByte(bRegister, value: Byte; times: Integer); overload;
+       function GetSerialNumber: String;
+       function ReadByte(bRegister: Byte): Byte;
+       procedure WriteByte(bRegister, Value: Byte); overload;
+       procedure WriteByte(bRegister, Value: Byte; Times: Integer); overload;
     public
       constructor Create;
       destructor Destroy; override;
@@ -76,9 +78,9 @@ type
       function GetBasicValue: Integer;
       function GetFanMode: TModeType;
       function GetScenario: TScenarioType;
-      function isECLoaded(bEC: Boolean): Boolean;
-      function isCoolerBoostEnabled: Boolean;
-      function isWebcamEnabled: Boolean;
+      function IsECLoaded(bEC: Boolean): Boolean;
+      function IsCoolerBoostEnabled: Boolean;
+      function IsWebcamEnabled: Boolean;
       procedure SetBasicMode(Value: Integer);
       procedure SetFanMode(mode: TModeType);
       procedure SetScenario(Scenario: TScenarioType; CpuFan0: Integer; GpuFan0: Integer; CPUFanSpeed: PFanSpeedArray; GPUFanSpeed: PFanSpeedArray); overload;
@@ -94,6 +96,7 @@ implementation
 constructor TMSIController.Create;
 var
   i, j, k: Integer;
+  serialNumber: String;
   bDummy: Byte;
 begin
   inherited Create;
@@ -102,8 +105,11 @@ begin
   hasEC := False;
   j := 0; k := 0;
 
+  serialNumber := self.GetSerialNumber;
+  if (not AnsiStartsStr(MSI_BIOS_SERIAL_NUMBER_PREFIX, serialNumber)) then Exit;
+
   for i := 1 to EC_LOADED_RETRY do begin
-    hasEC := EC.readByte(0, bDummy);
+    hasEC := EC.ReadByte(0, bDummy);
     if hasEC then Break else Sleep(1);
   end;
 
@@ -125,46 +131,69 @@ begin
 end;
 
 
-function TMSIController.readByte(bRegister: Byte): Byte;
+function TMSIController.GetSerialNumber: String;
+var
+  Locator: OleVariant;
+  Services: OleVariant;
+  WMIObjectSet: OleVariant;
+  Enum: IEnumVariant;
+  Value: LongWord;
+  Item: OleVariant;
 begin
-  Result := 255;
-  while (not EC.readByte(bRegister, Result)) or (Result = 255) do Sleep(1);
+  try
+    Result := '';
+    CoInitialize(nil);
+    Locator := CreateOleObject('WbemScripting.SWbemLocator');
+    Services := Locator.ConnectServer('.', 'root\CIMV2', '', '');
+    WMIObjectSet := Services.ExecQuery('SELECT SerialNumber FROM Win32_BIOS', 'WQL', 0);
+    Enum := IUnknown(WMIObjectSet._NewEnum) as IEnumVariant;
+    if (Enum.Next(1, Item, Value) = S_OK) then Result := Trim(Item.SerialNumber);
+  finally
+    CoUninitialize;
+  end;
 end;
 
 
-procedure TMSIController.writeByte(bRegister, value: Byte);
+function TMSIController.ReadByte(bRegister: Byte): Byte;
 begin
-  while (self.readByte(bRegister) <> value) do EC.writeByte(bRegister, value);
+  Result := 255;
+  while (not EC.ReadByte(bRegister, Result)) or (Result = 255) do Sleep(1);
+end;
+
+
+procedure TMSIController.WriteByte(bRegister, Value: Byte);
+begin
+  while (self.ReadByte(bRegister) <> value) do EC.WriteByte(bRegister, value);
   Sleep(1); //Do extra delay, sometimes it writes false positives
 end;
 
 
-procedure TMSIController.writeByte(bRegister, value: Byte; times: Integer);
+procedure TMSIController.WriteByte(bRegister, Value: Byte; Times: Integer);
 var
   i: Integer;
 begin
-  for i := 1 to times do self.writeByte(bRegister, value);
+  for i := 1 to Times do self.WriteByte(bRegister, value);
 end;
 
 
 function TMSIController.GetGPUTemp: Byte;
 begin
-  if (not self.isECLoaded(True)) then begin Result := 0; Exit; end;
-  Result := self.readByte(EC_GPU_TEMP_ADRRESS);
+  if (not self.IsECLoaded(True)) then begin Result := 0; Exit; end;
+  Result := self.ReadByte(EC_GPU_TEMP_ADRRESS);
 end;
 
 
 function TMSIController.GetCPUTemp: Byte;
 begin
-  if (not self.isECLoaded(True)) then begin Result := 0; Exit; end;
-  Result := self.readByte(EC_CPU_TEMP_ADRRESS);
+  if (not self.IsECLoaded(True)) then begin Result := 0; Exit; end;
+  Result := self.ReadByte(EC_CPU_TEMP_ADRRESS);
 end;
 
 
 function TMSIController.GetBasicValue: Integer;
 begin
-  if (not self.isECLoaded(True)) then begin Result := 128; Exit; end;
-  Result := self.readByte(EC_FANS_SPEED_ADRRESS);
+  if (not self.IsECLoaded(True)) then begin Result := 128; Exit; end;
+  Result := self.ReadByte(EC_FANS_SPEED_ADRRESS);
   if (Result >= 128) then Result := 128 - Result;
 end;
 
@@ -172,9 +201,9 @@ end;
 function TMSIController.GetFanMode: TModeType;
 begin
   Result := modeAuto;
-  if (not self.isECLoaded(True)) then Exit;
+  if (not self.IsECLoaded(True)) then Exit;
 
-  case self.readByte(EC_FANS_ADRRESS) of
+  case self.ReadByte(EC_FANS_ADRRESS) of
     EC_FANS_MODE_AUTO: Result := modeAuto;
     EC_FANS_MODE_BASIC: Result := modeBasic;
     EC_FANS_MODE_ADVANCED: Result := modeAdvanced;
@@ -187,18 +216,18 @@ var
   bResult: Byte;
 begin
   Result := scenarioBalanced;
-  if (not self.isECLoaded(True)) then Exit;
+  if (not self.IsECLoaded(True)) then Exit;
 
   Result := scenarioCoolerBoost;
-  if (self.isCoolerBoostEnabled()) then Exit;
+  if (self.IsCoolerBoostEnabled()) then Exit;
 
   //Check for two simple scenarios
-  bResult := self.readByte(EC_SCENARIO_ADDRESS);
+  bResult := self.ReadByte(EC_SCENARIO_ADDRESS);
   if (bResult = EC_SCENARIO_SILENT) then Result := scenarioSilent;
   if (bResult = EC_SCENARIO_BALANCED) then Result := scenarioBalanced;
 
   //If we have extreme mode, we need to check which subclass of it we have
-  if (bResult = EC_SCENARIO_EXTREME) then bResult := self.readByte(EC_FAN_MODE_ADDRESS);
+  if (bResult = EC_SCENARIO_EXTREME) then bResult := self.ReadByte(EC_FAN_MODE_ADDRESS);
   if (bResult = EC_FAN_MODE_AUTO) then Result := scenarioAuto;
   if (bResult = EC_FAN_MODE_ADVANCED) then Result := scenarioAdvanced;
 
@@ -219,60 +248,60 @@ begin
   self.SetCoolerBoostEnabled(Scenario = scenarioCoolerBoost);
 
   //Change scenario value, based on scenario
-  if (Scenario = scenarioSilent) then self.writeByte(EC_SCENARIO_ADDRESS, EC_SCENARIO_SILENT);
-  if (Scenario = scenarioBalanced) then self.writeByte(EC_SCENARIO_ADDRESS, EC_SCENARIO_BALANCED);
-  if (Scenario = scenarioAuto) then self.writeByte(EC_SCENARIO_ADDRESS, EC_SCENARIO_EXTREME);
-  if (Scenario = scenarioCoolerBoost) then self.writeByte(EC_SCENARIO_ADDRESS, EC_SCENARIO_EXTREME);
-  if (Scenario = scenarioAdvanced) then self.writeByte(EC_SCENARIO_ADDRESS, EC_SCENARIO_EXTREME);
+  if (Scenario = scenarioSilent) then self.WriteByte(EC_SCENARIO_ADDRESS, EC_SCENARIO_SILENT);
+  if (Scenario = scenarioBalanced) then self.WriteByte(EC_SCENARIO_ADDRESS, EC_SCENARIO_BALANCED);
+  if (Scenario = scenarioAuto) then self.WriteByte(EC_SCENARIO_ADDRESS, EC_SCENARIO_EXTREME);
+  if (Scenario = scenarioCoolerBoost) then self.WriteByte(EC_SCENARIO_ADDRESS, EC_SCENARIO_EXTREME);
+  if (Scenario = scenarioAdvanced) then self.WriteByte(EC_SCENARIO_ADDRESS, EC_SCENARIO_EXTREME);
 
   //Change fan mode, only if in extreme mode (Extreme mode contains: Auto, Cooler Boost, Advanced)
-  if (Scenario = scenarioAuto) then self.writeByte(EC_FAN_MODE_ADDRESS, EC_FAN_MODE_AUTO);
-  if (Scenario = scenarioAdvanced) then self.writeByte(EC_FAN_MODE_ADDRESS, EC_FAN_MODE_ADVANCED);
+  if (Scenario = scenarioAuto) then self.WriteByte(EC_FAN_MODE_ADDRESS, EC_FAN_MODE_AUTO);
+  if (Scenario = scenarioAdvanced) then self.WriteByte(EC_FAN_MODE_ADDRESS, EC_FAN_MODE_ADVANCED);
 
   //If custom fan data, then we need to write also cpu fan speeds data (can be used in any mode, but better to only use in advanced)
   if Assigned(CPUFanSpeed) then begin
-    self.writeByte(EC_FAN_1_1, Byte(CPUFanSpeed^[0]));
-    self.writeByte(EC_FAN_1_2, Byte(CPUFanSpeed^[1]));
-    self.writeByte(EC_FAN_1_3, Byte(CPUFanSpeed^[2]));
-    self.writeByte(EC_FAN_1_4, Byte(CPUFanSpeed^[3]));
-    self.writeByte(EC_FAN_1_5, Byte(CPUFanSpeed^[4]));
-    self.writeByte(EC_FAN_1_6, Byte(CPUFanSpeed^[5]));
+    self.WriteByte(EC_FAN_1_1, Byte(CPUFanSpeed^[0]));
+    self.WriteByte(EC_FAN_1_2, Byte(CPUFanSpeed^[1]));
+    self.WriteByte(EC_FAN_1_3, Byte(CPUFanSpeed^[2]));
+    self.WriteByte(EC_FAN_1_4, Byte(CPUFanSpeed^[3]));
+    self.WriteByte(EC_FAN_1_5, Byte(CPUFanSpeed^[4]));
+    self.WriteByte(EC_FAN_1_6, Byte(CPUFanSpeed^[5]));
   end;
 
   //If custom fan speeds, then we need to write also gpu fan speeds data (can be used in any mode, but better to only use in advanced)
   if Assigned(GPUFanSpeed) then begin
-    self.writeByte(EC_FAN_2_1, Byte(GPUFanSpeed^[0]));
-    self.writeByte(EC_FAN_2_2, Byte(GPUFanSpeed^[1]));
-    self.writeByte(EC_FAN_2_3, Byte(GPUFanSpeed^[2]));
-    self.writeByte(EC_FAN_2_4, Byte(GPUFanSpeed^[3]));
-    self.writeByte(EC_FAN_2_5, Byte(GPUFanSpeed^[4]));
-    self.writeByte(EC_FAN_2_6, Byte(GPUFanSpeed^[5]));
+    self.WriteByte(EC_FAN_2_1, Byte(GPUFanSpeed^[0]));
+    self.WriteByte(EC_FAN_2_2, Byte(GPUFanSpeed^[1]));
+    self.WriteByte(EC_FAN_2_3, Byte(GPUFanSpeed^[2]));
+    self.WriteByte(EC_FAN_2_4, Byte(GPUFanSpeed^[3]));
+    self.WriteByte(EC_FAN_2_5, Byte(GPUFanSpeed^[4]));
+    self.WriteByte(EC_FAN_2_6, Byte(GPUFanSpeed^[5]));
   end;
 
   //If no custom fan speeds, and mode isn't advanced, then use default ones
   if (not Assigned(CPUFanSpeed) and (Scenario <> scenarioAdvanced)) then begin
-    self.writeByte(EC_FAN_1_1, EC_DEFAULT_CPU_FAN_SPEED[0]);
-    self.writeByte(EC_FAN_1_2, EC_DEFAULT_CPU_FAN_SPEED[1]);
-    self.writeByte(EC_FAN_1_3, EC_DEFAULT_CPU_FAN_SPEED[2]);
-    self.writeByte(EC_FAN_1_4, EC_DEFAULT_CPU_FAN_SPEED[3]);
-    self.writeByte(EC_FAN_1_5, EC_DEFAULT_CPU_FAN_SPEED[4]);
-    self.writeByte(EC_FAN_1_6, EC_DEFAULT_CPU_FAN_SPEED[5]);
+    self.WriteByte(EC_FAN_1_1, EC_DEFAULT_CPU_FAN_SPEED[0]);
+    self.WriteByte(EC_FAN_1_2, EC_DEFAULT_CPU_FAN_SPEED[1]);
+    self.WriteByte(EC_FAN_1_3, EC_DEFAULT_CPU_FAN_SPEED[2]);
+    self.WriteByte(EC_FAN_1_4, EC_DEFAULT_CPU_FAN_SPEED[3]);
+    self.WriteByte(EC_FAN_1_5, EC_DEFAULT_CPU_FAN_SPEED[4]);
+    self.WriteByte(EC_FAN_1_6, EC_DEFAULT_CPU_FAN_SPEED[5]);
   end;
 
   //If no custom fan speeds, and mode isn't advanced, then use default ones
   if (not Assigned(CPUFanSpeed) and (Scenario <> scenarioAdvanced)) then begin
-    self.writeByte(EC_FAN_2_1, EC_DEFAULT_GPU_FAN_SPEED[0]);
-    self.writeByte(EC_FAN_2_2, EC_DEFAULT_GPU_FAN_SPEED[1]);
-    self.writeByte(EC_FAN_2_3, EC_DEFAULT_GPU_FAN_SPEED[2]);
-    self.writeByte(EC_FAN_2_4, EC_DEFAULT_GPU_FAN_SPEED[3]);
-    self.writeByte(EC_FAN_2_5, EC_DEFAULT_GPU_FAN_SPEED[4]);
-    self.writeByte(EC_FAN_2_6, EC_DEFAULT_GPU_FAN_SPEED[5]);
+    self.WriteByte(EC_FAN_2_1, EC_DEFAULT_GPU_FAN_SPEED[0]);
+    self.WriteByte(EC_FAN_2_2, EC_DEFAULT_GPU_FAN_SPEED[1]);
+    self.WriteByte(EC_FAN_2_3, EC_DEFAULT_GPU_FAN_SPEED[2]);
+    self.WriteByte(EC_FAN_2_4, EC_DEFAULT_GPU_FAN_SPEED[3]);
+    self.WriteByte(EC_FAN_2_5, EC_DEFAULT_GPU_FAN_SPEED[4]);
+    self.WriteByte(EC_FAN_2_6, EC_DEFAULT_GPU_FAN_SPEED[5]);
   end;
 
   //We can change these values, to rapidly stop or accelerate fans
   //Attempt writing multiple times, to ensure correct result
-  if (CpuFan0 > -1) then self.writeByte(EC_FAN_1_0, CpuFan0, 2);
-  if (GpuFan0 > -1) then self.writeByte(EC_FAN_2_0, GpuFan0, 2);
+  if (CpuFan0 > -1) then self.WriteByte(EC_FAN_1_0, CpuFan0, 2);
+  if (GpuFan0 > -1) then self.WriteByte(EC_FAN_2_0, GpuFan0, 2);
 end;
 
 
@@ -282,73 +311,73 @@ begin
 end;
 
 
-function TMSIController.isECLoaded(bEC: Boolean): Boolean;
+function TMSIController.IsECLoaded(bEC: Boolean): Boolean;
 begin
   Result := ((not bEC) or hasEC) and EC.driverFileExist and EC.driverLoaded;
 end;
 
 
-function TMSIController.isCoolerBoostEnabled: Boolean;
+function TMSIController.IsCoolerBoostEnabled: Boolean;
 begin
-  if (not self.isECLoaded(True)) then begin Result := False; Exit; end;
-  Result := (self.readByte(EC_CB_ADDRESS) = EC_CB_ON);
+  if (not self.IsECLoaded(True)) then begin Result := False; Exit; end;
+  Result := (self.ReadByte(EC_CB_ADDRESS) = EC_CB_ON);
 end;
 
 
-function TMSIController.isWebcamEnabled: Boolean;
+function TMSIController.IsWebcamEnabled: Boolean;
 begin
-  if (not self.isECLoaded(True)) then begin Result := False; Exit; end;
-  Result := (self.readByte(EC_WEBCAM_ADDRESS) = EC_WEBCAM_ON);
+  if (not self.IsECLoaded(True)) then begin Result := False; Exit; end;
+  Result := (self.ReadByte(EC_WEBCAM_ADDRESS) = EC_WEBCAM_ON);
 end;
 
 
 procedure TMSIController.SetBasicMode(Value: Integer);
 begin
-  if (not self.isECLoaded(True)) then Exit;
+  if (not self.IsECLoaded(True)) then Exit;
   if (Value < -15) or (Value > 15) then Exit;
   if (Value <= 0) then Value := 128 + Abs(Value);
   SetFanMode(modeBasic);
-  self.writeByte(EC_FANS_SPEED_ADRRESS, Value);
+  self.WriteByte(EC_FANS_SPEED_ADRRESS, Value);
 end;
 
 
 procedure TMSIController.SetFanMode(mode: TModeType);
 begin
-  if (not self.isECLoaded(True)) then Exit;
+  if (not self.IsECLoaded(True)) then Exit;
 
   case mode of
-    modeAuto: self.writeByte(EC_FANS_ADRRESS, EC_FANS_MODE_AUTO);
-    modeBasic: self.writeByte(EC_FANS_ADRRESS, EC_FANS_MODE_BASIC);
-    modeAdvanced: self.writeByte(EC_FANS_ADRRESS, EC_FANS_MODE_ADVANCED);
+    modeAuto: self.WriteByte(EC_FANS_ADRRESS, EC_FANS_MODE_AUTO);
+    modeBasic: self.WriteByte(EC_FANS_ADRRESS, EC_FANS_MODE_BASIC);
+    modeAdvanced: self.WriteByte(EC_FANS_ADRRESS, EC_FANS_MODE_ADVANCED);
   end;
 end;
 
 
 procedure TMSIController.SetCoolerBoostEnabled(bool: Boolean);
 begin
-  if (not self.isECLoaded(True)) then Exit;
-  if bool then self.writeByte(EC_CB_ADDRESS, EC_CB_ON);
-  if not bool then self.writeByte(EC_CB_ADDRESS, EC_CB_OFF);
+  if (not self.IsECLoaded(True)) then Exit;
+  if bool then self.WriteByte(EC_CB_ADDRESS, EC_CB_ON);
+  if not bool then self.WriteByte(EC_CB_ADDRESS, EC_CB_OFF);
 end;
 
 
 procedure TMSIController.SetWebcamEnabled(bool: Boolean);
 begin
-  if (not self.isECLoaded(True)) then Exit;
-  if bool then self.writeByte(EC_WEBCAM_ADDRESS, EC_WEBCAM_ON);
-  if not bool then self.writeByte(EC_WEBCAM_ADDRESS, EC_WEBCAM_OFF);
+  if (not self.IsECLoaded(True)) then Exit;
+  if bool then self.WriteByte(EC_WEBCAM_ADDRESS, EC_WEBCAM_ON);
+  if not bool then self.WriteByte(EC_WEBCAM_ADDRESS, EC_WEBCAM_OFF);
 end;
 
 
 procedure TMSIController.ToggleCoolerBoost;
 begin
-  SetCoolerBoostEnabled(not isCoolerBoostEnabled);
+  SetCoolerBoostEnabled(not IsCoolerBoostEnabled);
 end;
 
 
 procedure TMSIController.ToggleWebcam;
 begin
-  SetWebcamEnabled(not isWebcamEnabled);
+  SetWebcamEnabled(not IsWebcamEnabled);
 end;
 
 end.
